5540e29b73a9c0f1e4f11d4f40b8a05c4688e1ab
Abdul Derh <abdul.derh@gmail.com>
2022-03-28 15:35:39 -0400

    Reafactor

---
+++ front-end/src/routes/intro/Login.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import TypeWriter from 'svelte-typewriter';
+	import { fade } from 'svelte/transition';
+</script>
+
+<div
+	class="block w-72 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center"
+	in:fade
+	out:fade
+>
+	<img
+		alt=""
+		class="rounded-full h-48 mx-auto"
+		src="https://media-exp1.licdn.com/dms/image/C4E03AQEVw3Y5-jGqSw/profile-displayphoto-shrink_800_800/0/1632075240706?e=1652918400&v=beta&t=sCkD7V8GWNn6ik1gazqmJvZDehUWNfFHKi46g9eLEbM"
+	/>
+	<h1 class="text-5xl my-3">Abdul Derh</h1>
+	<div
+		class="mt-7 bg-white text-left h-7 w-full rounded-sm p-1 text-black overflow-hidden resize-none editable"
+		type="password"
+		placeholder="Password"
+	>
+		<TypeWriter delay="1000">**********</TypeWriter>
+	</div>
+	<p class="text-gray-300 mt-5">Reset Password</p>
+	<p class="text-gray-300 mt-5">Sign-in Options</p>
+</div>
---
+++ front-end/src/routes/intro/Pointer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import FaMousePointer from 'svelte-icons/fa/FaMousePointer.svelte';
+
+	export let x: Number | String = 0;
+	export let y: Number | String = 0;
+
+	export const pointer = {
+		movePointer: (new_x, new_y) => {
+			x = new_x;
+			y = new_y;
+		}
+	};
+</script>
+
+<div class="cursor h-5 w-5 absolute" style="top: {y}; left: {x};">
+	<FaMousePointer />
+</div>
+
+<style>
+	.cursor {
+		transition-property: top left;
+		transition-duration: 1s;
+	}
+</style>
